de47699d7a7ba306a5b518fa085dedf8a3bd6575
Rakibul Islam <rakibul.islam7772588@gmail.com>
2020-11-07 19:07:49 +0600

    [ADD] added matplotlib

---
+++ sales/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from .models import Sale
+from .utils import get_plot
+
+# Create your views here.
+def main_view(request):
+    qs = Sale.objects.all()
+    x = [x.item for x in qs]
+    y = [y.price for y in qs]
+    chart = get_plot(x, y)
+
+    context = {
+        'chart': chart
+    }
+
+    return render(request, 'sales/plot.html', context)
